fc25fa7533bc92f148bf35406565644d3837c7be
Jacek Ziętek <22346340+jzietek@users.noreply.github.com>
2022-03-23 23:23:14 +0100

    Removed not-supported backlight toggling

--- src/flask_app.py
+++ src/flask_app.py
@@ -2,13 +2,13 @@
 from cgitb import text
 import re
 from flask import Flask, request, jsonify
-from grove_api import lcd_stub
+from grove_api import lcd
 
 app = Flask(__name__)
 
 @app.get("/lcd")
 def get_lcd_text():
-    return jsonify(lcd_stub.get_displayed_text())
+    return jsonify(lcd.get_displayed_text())
 
 
 @app.post("/lcd")
@@ -18,24 +18,12 @@ def update_lcd_text():
         cursor_row = request_json["cursorRow"]
         cursor_column = request_json["cursorColumn"]
         text = request_json["text"]
-        lcd_stub.set_displayed_text(cursor_row, cursor_column, text)
+        lcd.set_displayed_text(cursor_row, cursor_column, text)
         return request_json, 201
     return {"error": "Request must be JSON"}, 415
 
 
 @app.delete("/lcd")
 def delete_lcd_text():
-    lcd_stub.clear_displayed_text()
+    lcd.clear_displayed_text()
     return "LCD content cleared"
-
-
-@app.post("/lcd/backlightOn")
-def backlight_enable():
-    lcd_stub.toggle_backlight(True)
-    return "Backlight enabled"
-
-
-@app.post("/lcd/backlightOff")
-def backlight_disable():
-    lcd_stub.toggle_backlight(False)
-    return "Backlight disabled"
