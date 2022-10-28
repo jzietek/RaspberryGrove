fa461ea55ecb30dbf0cd02ec680e7f9a46eec531
Jacek Zietek <j.zietek@gmail.com>
2022-10-28 20:15:38 +0100

    Nohup experiments

---
+++ python/src/watcher_light.sh
@@ -0,0 +1,1 @@
+ python3 ./watcher_light.py http://raspberrypi:8080 -i 1 -t 5
---
+++ python/src/watcher_light_deamon.sh
@@ -0,0 +1,1 @@
+nohup ./watcher_light.py http://raspberrypi:8080 -i 1 -t 5 > ../logs/watcher_light.log 2> ../logs/watcher_light.err < /dev/null &
