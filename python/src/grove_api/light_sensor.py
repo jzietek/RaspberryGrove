a89a338d4697fa6d28ac9f1a2c7286c039994a5e
Jacek Ziętek <22346340+jzietek@users.noreply.github.com>
2022-10-31 08:37:11 +0100

    Refactored light_sensor

--- python/src/grove_api/light_sensor.py
+++ python/src/grove_api/light_sensor.py
@@ -2,23 +2,24 @@ import random
 import logging
 
 class LightSensor(object):
+    """
+    This is an API for the Grove Light Sensor v1.2:
+    https://seeeddoc.github.io/Grove-Light_Sensor_v1.2/
+    """
     def __init__(self, analog_port_number = 0):
         self.__logger = logging.getLogger(__name__)
-        self.__init_sensor()
-        if (self.__grove_available):
-            self.__sensor = GroveLightSensor(analog_port_number)
-        else:
-            self.__sensor = None
-
-    def __init_sensor(self):
         try:
             from grove.grove_light_sensor_v1_2 import GroveLightSensor
             self.__logger.info('Grove light sensor is installed')
-            (self.__grove_available) = True
+            self.__sensor = GroveLightSensor(analog_port_number)
         except ImportError:
             self.__logger.warning("Grove not supported. Using mocked light sensor instead.")
-            (self.__grove_available) = False
+            self.__sensor = None
 
+    """
+    Reads current luminosity in LUX units measured by the sensor.
+    If no sensor is installed, random value will be returned from range 0 .. 100.
+    """
     def read_light_intensity(self):
         if (self.__sensor is not None):
             return self.__sensor.light
