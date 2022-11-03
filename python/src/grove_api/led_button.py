b706e5595eaac307be24e5cd63acd8a1c8cc5825
Jacek Ziętek <22346340+jzietek@users.noreply.github.com>
2022-11-03 21:38:21 +0100

    Led button testing

--- python/src/grove_api/led_button.py
+++ python/src/grove_api/led_button.py
@@ -14,14 +14,14 @@ class LedButton(object):
     def __init__(self, digital_port_number, button_index = 0):
         self.__logger = logging.getLogger(__name__)
         self.button_pressed_event = ButtonEvent(button_index)
-        self.button_long_pressed_event = ButtonEvent(button_index)
-        self.button_double_pressed_event = ButtonEvent(button_index)
+        #self.button_long_pressed_event = ButtonEvent(button_index)
+        #self.button_double_pressed_event = ButtonEvent(button_index)
         try:
             from grove.button import Button
             from grove.grove_ryb_led_button import GroveLedButton
             self.__logger.info('Grove LED button is installed')
             self.__button = GroveLedButton(digital_port_number)
-            self.__button_constants = Button
+            self.__button_constants = Button()
             self.enable_light(False)
             self.__button.on_event = self.__handle_button_event
         except:
@@ -34,13 +34,13 @@ class LedButton(object):
             if event & self.__button_constants.EV_SINGLE_CLICK:
                 #self.__logger.info('single click')
                 #self.__button.led.light(True)
-                self.button_pressed_event()
-            elif event & self.__button_constants.EV_LONG_PRESS:
+                self.button_pressed_event('single click')
+            #elif event & self.__button_constants.EV_LONG_PRESS:
                 #self.__logger.info('long press')
                 #self.__button.led.light(False)
-                self.button_long_pressed_event()
-            elif event & self.__button_constants.EV_DOUBLE_CLICK:
-                self.button_double_pressed_event()
+                #self.button_long_pressed_event()
+            #elif event & self.__button_constants.EV_DOUBLE_CLICK:
+                #self.button_double_pressed_event()
 
 
     def enable_light(self, isEnabled):
@@ -52,14 +52,15 @@ class LedButton(object):
     def toggle_ligth(self):
         self.enable_light(not self.is_light_on)
         
+def test1(text):
+    print(text)
 
 if (__name__ == '__main__'):
     led_button = LedButton(5)
-    led_button.button_pressed_event += print('button pressed')
-    led_button.button_long_pressed_event += print('button long pressed')    
-    led_button.button_double_pressed_event += print('button double pressed')
-
-    led_button.button_long_pressed_event += led_button.toggle_ligth
+    led_button.button_pressed_event += test1
+    #led_button.button_long_pressed_event += print('button long pressed')    
+    #led_button.button_double_pressed_event += print('button double pressed')
+    #led_button.button_long_pressed_event += led_button.toggle_ligth
 
     while True:
         time.sleep(1)
